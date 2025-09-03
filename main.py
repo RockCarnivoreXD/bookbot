776ceed279eb3053d4acf686f5819b9efde773ff
RockCarnivoreXD <rockcarnivorexd@gmail.com>
2025-09-03 14:10:35 -0600

    word count function moved to stats.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
+from stats import get_num_words
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
-def word_counter(book_text):
-    word_count = len(book_text.split())
-    return word_count
-
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    num_words = word_counter(book_text)
-    word_message = f"{num_words} words found in the document"
+    word_count = get_num_words(book_text)
+    word_message = f"{word_count} words found in the document"
     print(word_message)
 main()
 
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(book_text):
+    word_count = len(book_text.split())
+    return word_count
