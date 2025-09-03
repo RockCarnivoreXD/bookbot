107425b754c9a9e555151a78a1970d3392335191
RockCarnivoreXD <rockcarnivorexd@gmail.com>
2025-09-03 13:58:33 -0600

    word counter working

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
+
+def word_counter(book_text):
+    word_count = len(book_text.split())
+    return word_count
+
 def main():
-    
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    num_words = word_counter(book_text)
+    word_message = f"{num_words} words found in the document"
+    print(word_message)
 main()
+
+
+
+
+
+
