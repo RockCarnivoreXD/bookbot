34d6e7bb091110f4892e1366645953f23916d6bc
RockCarnivoreXD <rockcarnivorexd@gmail.com>
2025-09-04 12:44:27 -0600

    Count Characters lesson completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import get_char_count
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,7 +10,10 @@ def main():
     book_text = get_book_text("books/frankenstein.txt")
     word_count = get_num_words(book_text)
     word_message = f"{word_count} words found in the document"
+    char_count = get_char_count(book_text)
     print(word_message)
+    print(char_count)
+
 main()
 
 
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_num_words(book_text):
     word_count = len(book_text.split())
     return word_count
+
+def get_char_count(book_text):
+    char_count = {} 
+    low_case_words = book_text.lower()
+    for char in low_case_words:
+        if char in char_count:
+            char_count[char] += 1
+        else: 
+            char_count[char] = 1
+    return char_count
