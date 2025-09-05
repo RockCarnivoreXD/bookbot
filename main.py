4de2f8aab3939e984d8c1a2d4b83e74d5b7cafe8
RockCarnivoreXD <rockcarnivorexd@gmail.com>
2025-09-05 12:05:58 -0600

    Arguments lesson completed

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words
 from stats import get_char_count
 from stats import get_sorted_char_counts
@@ -10,19 +11,23 @@ def get_book_text(filepath):
 def print_structure(char_count_list):
     for char in char_count_list:
         if  char["char"].isalpha():
-            print(f"{char["char"]}: {char["num"]}")
+            print(f"{char['char']}: {char['num']}")
         else:
             continue
 
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    usr_fl_path = sys.argv[1]
+    book_text = get_book_text(usr_fl_path)
     word_count = get_num_words(book_text)
     word_message = f" Found {word_count} total words"
     char_count = get_char_count(book_text)
     sorted_char_count = get_sorted_char_counts(char_count)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {usr_fl_path}...")
     print("----------- Word Count ----------")
     print(word_message)
     print("--------- Character Count -------")
@@ -31,9 +36,3 @@ def main():
     
 
 main()
-
-
-
-
-
-
